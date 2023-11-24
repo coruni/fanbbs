ec5e024a8542f35620cfdf513c29aceef884e009
maplene <mineimc@outlook.com>
2023-11-24 14:45:12 +0800

    完善分类页面

--- pages/common/category/categoryInfo.vue
+++ pages/common/category/categoryInfo.vue
@@ -1,17 +1,19 @@
 <template>
 	<view>
-		<z-paging refresher-only @onRefresh="onRefresh" ref="paging" style="background: #faead7;">
+		<z-paging refresher-only @onRefresh="onRefresh" ref="paging" :style="{background:info.opt&& info.opt.primary}"
+			@scroll="onScroll">
 			<template #top>
-				<u-navbar autoBack bgColor="transparent"></u-navbar>
+				<u-navbar autoBack :bgColor="$u.colorToRgba(info.opt && info.opt.primary,opacity)" fixed></u-navbar>
 			</template>
-			<view class="info">
-				<image :src="info.imgurl" mode="aspectFill" style="width: 100%;height: 550rpx;"></image>
+			<view class="info" id="infoPanel">
+				<image :src="info.imgurl" mode="aspectFill" style="width: 100%;height: 600rpx;"></image>
 				<view style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: #faead778;">
 					<view class="info_lay"
-						style="background: linear-gradient(to top, #faead79d 20%, rgba(0, 0, 0, 0) 70%);">
+						:style="`background: linear-gradient(to top, ${info.opt && $u.colorToRgba(info.opt.primary,0.8)} 30%, rgba(0, 0, 0, 0) 70%);`">
 						<view style="margin:30rpx;margin-bottom: 60rpx;">
 							<u-row>
-								<u-avatar :src="info.imgurl" size="70" shape="square" mode="aspectFill"></u-avatar>
+								<u-avatar :src="info.imgurl" size="70" shape="square" mode="aspectFill"
+									customStyle="border-radius:20rpx"></u-avatar>
 								<view style="display: flex;flex-direction: column;margin-left: 30rpx;">
 									<text style="font-size: 36rpx;color: white;">{{info.name}}</text>
 									<text>占位123</text>
@@ -21,19 +23,34 @@
 								<text>{{info.description}}</text>
 							</view>
 							<view style="margin-top: 30rpx;">
-								<u-button shape="circle" color="#faead7">加入</u-button>
+								<u-button shape="circle" :color="info.opt&& info.opt.primary">加入</u-button>
 							</view>
 						</view>
 					</view>
 				</view>
 			</view>
-			<view style="position: relative;top: -30rpx;background: #faead7;border-radius: 40rpx 40rpx 0 0;">
-				<u-sticky bgColor="#faead7">
-					<u-tabs :list="list" style="margin: 0 15rpx;" lineColor="#dccebf"></u-tabs>
+			<view style="position: relative;top: -30rpx;border-radius: 40rpx 40rpx 0 0;"
+				:style="{background:info.opt && info.opt.primary}">
+				<!-- #ifndef APP -->
+				<u-sticky :bgColor="isSticky?info.opt&& info.opt.primary:''">
+					<u-tabs :list="list" :current="tabsIndex" @change="tabsIndex = $event.index" style="margin: 0 15rpx;" lineColor="#dccebf"></u-tabs>
 				</u-sticky>
-				<swiper style="height: 100vh;">
-					<swiper-item v-for="(item,index) in list" :key="index">
-						{{info}}
+				<!-- #endif -->
+
+				<!-- #ifdef APP -->
+				<u-sticky :bgColor="isSticky?'#faead7':''" offsetTop="60">
+					<u-tabs :list="list" :current="tabsIndex" @change="tabsIndex = $event.index"
+						style="margin: 0 15rpx;" lineColor="#dccebf"></u-tabs>
+				</u-sticky>
+				<!-- #endif -->
+
+				<swiper style="height: 100vh;" :current="tabsIndex"
+					@animationfinish="tabsIndex = $event.detail.current">
+					<swiper-item v-if="id">
+						<hot-article :mid="id" :isScroll="isScroll"></hot-article>
+					</swiper-item>
+					<swiper-item v-if="id">
+						<new-article :mid="id" :isScroll="isScroll"></new-article>
 					</swiper-item>
 				</swiper>
 			</view>
@@ -42,11 +59,23 @@
 </template>
 
 <script>
+	import hotArticle from './components/hot.vue';
+	import newArticle from './components/new.vue';
 	export default {
+		components: {
+			hotArticle,
+			newArticle
+		},
 		data() {
 			return {
 				id: 0,
 				info: {},
+				isScroll: false,
+				isSticky: false,
+				panelHeight: 0,
+				platform: '',
+				opacity: 0,
+				tabsIndex: 0,
 				list: [{
 						name: '热门'
 
@@ -58,7 +87,18 @@
 			};
 		},
 		onLoad(params) {
+			this.id = params.id
 			this.getData(params.id)
+			this.platform = uni.getSystemInfoSync().uniPlatform
+		},
+		onReady() {
+			this.$nextTick(() => {
+				uni.createSelectorQuery().in(this).select("#infoPanel").boundingClientRect(data => {
+					this.panelHeight = data.height
+					console.log(data.height)
+				}).exec()
+			}, 200)
+
 		},
 		methods: {
 			getData(id) {
@@ -70,6 +110,7 @@
 					console.log(res)
 					if (res.data.code) {
 						this.info = res.data.data
+						console.log(uni.$u.hexToRgb(this.info.opt.primary))
 					}
 				})
 			},
@@ -77,6 +118,17 @@
 				setTimeout(() => {
 					this.$refs.paging.complete()
 				}, 1000)
+			},
+			onScroll(data) {
+				let scrollTop = data.detail.scrollTop
+				this.opacity = scrollTop / 100;
+				if (scrollTop > this.panelHeight - 44 - (this.platform == 'app' ? 45 : 0)) {
+					this.isScroll = true;
+					this.isSticky = true;
+				} else {
+					this.isSticky = false;
+					this.isScroll = false;
+				}
 			}
 		}
 	}
@@ -86,7 +138,7 @@
 	.info {
 		position: relative;
 		top: 0;
-		height: 550rpx;
+		height: 600rpx;
 
 		&_lay {
 			position: absolute;
---
+++ pages/common/category/components/hot.vue
@@ -0,0 +1,85 @@
+<template>
+	<view>
+		<z-paging @query="getData" ref="paging" v-model="article" :scrollable="scroll" :refresher-enabled="false">
+			<block v-for="(item,index) in article" :key="index">
+				<view @tap.stop="goArticle(item)" style="margin:30rpx 30rpx 0rpx 30rpx;padding-bottom: 10rpx;">
+					<article-header :data="item"></article-header>
+					<article-content :data="item"></article-content>
+					<article-footer :data="item"></article-footer>
+				</view>
+				<view style="border-bottom:1rpx #f7f7f7 solid"></view>
+			</block>
+		</z-paging>
+	</view>
+</template>
+
+<script>
+	import articleHeader from '@/components/article/header.vue';
+	import articleContent from '@/components/article/content.vue';
+	import articleFooter from '@/components/article/footer.vue';
+	export default {
+		name: 'hotArticle',
+		props: {
+			mid: {
+				type: [String, Number],
+				default: 0
+			},
+			isScroll: {
+				type: Boolean,
+				default: false,
+			}
+
+		},
+		watch: {
+			isScroll: {
+				handler(e) {
+					this.scroll = e
+				},
+				deep: true,
+				immediate: true,
+			}
+		},
+		components: {
+			articleHeader,
+			articleContent,
+			articleFooter,
+		},
+		data() {
+			return {
+				article: [],
+				scroll: false,
+			}
+		},
+		methods: {
+			getData(page, limit) {
+				this.$http.get('/typechoContents/contentsList', {
+					params: {
+						page,
+						limit,
+						searchParams: JSON.stringify({
+							mid: this.mid,
+							type: 'post'
+						}),
+						random: 1,
+						uid: this.$store.state.hasLogin ? this.$store.state.userInfo.uid : 0,
+						order: 'likes desc,replyTime desc,text desc,views desc,created desc'
+					}
+				}).then(res => {
+					this.$refs.paging.complete(res.data.data)
+				})
+			},
+			goArticle(data) {
+				uni.setStorageSync(`article_${data.cid}`, data)
+				this.$Router.push({
+					path: '/pages/article/article',
+					query: {
+						id: data.cid
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
