28007790a8a1af030bab0579503d1e75bdc2ba3e
maplene <mineimc@outlook.com>
2023-11-25 02:24:40 +0800

    优化关注按钮

--- components/article/header.vue
+++ components/article/header.vue
@@ -18,7 +18,7 @@
 
 			<view style="display: flex;align-items: center;">
 				<view @click.stop="follow(data.authorId)">
-					<u-button v-if="!data.isfollow && data.authorId != userInfo.uid" plain color="#a899e6" size="mini"
+					<u-button v-if="!isfollow && data.authorId != userInfo.uid" plain color="#a899e6" size="mini"
 						shape="circle" customStyle="font-size:28rpx;height:50rpx">关注</u-button>
 				</view>
 				<view @click.stop="">
@@ -70,7 +70,6 @@
 				})
 			},
 			follow(id) {
-				console.log(this.data.authorId,this.userInfo.uid)
 				if (this.userInfo.uid == id) return;
 				this.$http.post('/typechoUsers/follow', {
 					touid: id
