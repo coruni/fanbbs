a644bdadccb037a73231aee96ed4cc6498ed09a8
maplene <mineimc@outlook.com>
2024-03-07 23:40:49 +0800

    添加抽奖

---
+++ pagesA/lottery/lottery.vue
@@ -0,0 +1,132 @@
+<template>
+	<view :style="{height:height+'px'}" class="content">
+		<u-navbar title="幸运抽奖" autoBack bgColor="transparent">
+			<view slot="left">
+				<i class="ess mgc_left_line" style="font-size: 60rpx;"></i>
+			</view>
+		</u-navbar>
+		<LuckyGrid ref="lottery" width="600rpx" height="600rpx" :rows="3" :cols="3" :buttons="buttons" :blocks="blocks"
+			:prizes="prizes" :defaultStyle="defaultStyle" @start="play()">
+		</LuckyGrid>
+	</view>
+</template>
+
+<script>
+	import LuckyGrid from '@lucky-canvas/uni/lucky-grid' // 九宫格;
+	export default {
+		components: {
+			LuckyGrid
+		},
+
+		data() {
+			return {
+				height: 0,
+				blocks: [{}],
+				prizes: [],
+				buttons: [{
+					x: 1,
+					y: 1,
+					fonts: [{
+						text: '抽奖',
+						fontColor: 'white',
+						top: '45%'
+					}]
+
+				}],
+				defaultStyle: {
+					background: '#b8c5f2',
+
+				},
+				lottery: null,
+			};
+		},
+		onLoad() {
+			this.getData()
+			this.height = uni.getSystemInfoSync().windowHeight
+		},
+		methods: {
+			getData() {
+				const typeToImage = {
+				    'point': '/static/coin.png',
+				    'vip': '/static/vip.png',
+				};
+				this.$http.get('/raffle/list', {
+					page: 1,
+					limit: 8
+				}).then(res => {
+					if (res.data.code == 200) {
+						const items = res.data.data.data;
+						const gridSize = 3; 
+						const prizes = [];
+						items.forEach((item, index) => {
+							const row = Math.floor(index / gridSize);
+							const col = Math.floor(index % gridSize);
+							const src = typeToImage[item.type] || item.image;
+							if (row == 1 && col == 1) {
+								return; // 跳过 x 和 y 都为 1 的情况
+							}
+							prizes.push({
+								x: col,
+								y: row,
+								id: item.id,
+								fonts: [{
+									text: item.type == 'point' ? `${item.value}积分` : item
+										.type == 'vip'?`会员${item.value}天`:'实物',
+									fontColor: 'white',
+									fontSize: '16px',
+									top: '70%'
+
+								}],
+								imgs: [{
+									src,
+									width: '40%',
+									top: '30%'
+								}]
+							});
+						});
+
+						this.prizes = prizes;
+						console.log(prizes);
+					}
+				});
+			},
+			async play() {
+				this.$refs.lottery.play()
+				let data = await this.raffle()
+				let prizeIndex = this.findPrizeIndex(data.id);
+				setTimeout(() => {
+					this.$refs.lottery.stop(prizeIndex)
+				}, 3000)
+			},
+			raffle() {
+				return new Promise((resolve, reject) => {
+					this.$http.post('/raffle/raffle').then(res => {
+						console.log(res)
+						resolve(res.data.data)
+					})
+				})
+			},
+			findPrizeIndex(id) {
+				let prizes = [...this.prizes]; // 奖品数据数组
+				for (let i = 0; i < prizes.length; i++) {
+					if (prizes[i].id === id) {
+						return i; // 返回找到的奖品索引
+					}
+				}
+
+				return -1; // 如果没有找到匹配的id，返回-1
+			}
+
+		}
+
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
